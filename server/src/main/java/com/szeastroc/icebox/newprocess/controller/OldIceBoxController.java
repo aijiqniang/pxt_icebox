b450fdd9d676c838723b73cb92062c9da6ac79b3
cuimengyang <cuimengyang@szeastroc.com>
2020-07-14 17:26:32 +0800

    feat:旧冰柜导入接口修改，适配配送商

--- server/src/main/java/com/szeastroc/icebox/newprocess/controller/OldIceBoxController.java
+++ server/src/main/java/com/szeastroc/icebox/newprocess/controller/OldIceBoxController.java
@@ -4,12 +4,15 @@ import cn.hutool.core.date.DateTime;
 import cn.hutool.core.date.DateUtil;
 import cn.hutool.poi.excel.ExcelReader;
 import cn.hutool.poi.excel.WorkbookUtil;
+import com.alibaba.fastjson.JSON;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.szeastroc.common.constant.Constants;
 import com.szeastroc.common.exception.ImproperOptionException;
 import com.szeastroc.common.utils.FeignResponseUtil;
 import com.szeastroc.common.vo.CommonResponse;
 import com.szeastroc.customer.client.FeignStoreClient;
+import com.szeastroc.customer.client.FeignSupplierClient;
+import com.szeastroc.customer.common.vo.SimpleSupplierInfoVo;
 import com.szeastroc.customer.common.vo.StoreInfoDtoVo;
 import com.szeastroc.icebox.newprocess.dao.IceBoxDao;
 import com.szeastroc.icebox.newprocess.dao.IceBoxExtendDao;
@@ -27,13 +30,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Slf4j
 @RestController
@@ -47,6 +47,8 @@ public class OldIceBoxController {
     private IceBoxExtendDao iceBoxExtendDao;
     @Autowired
     private IceModelDao iceModelDao;
+    @Autowired
+    private FeignSupplierClient feignSupplierClient;
 
     @RequestMapping("/import")
     @Transactional(rollbackFor = Exception.class, value = "transactionManager")
@@ -73,74 +75,98 @@ public class OldIceBoxController {
 //        log.info("map的大小-->[{}]",map.size());
 
         log.info("开始处理数据");
+        List<Object> list = new ArrayList<>();
         for (int i = 0, readsSize = reads.size(); i < readsSize; i++) {
             log.info("---------------第" + i + "次循环---------------");
             List<Object> x = reads.get(i);
             String s = x.get(6).toString();
             if (StringUtils.isNotBlank(s)) {
                 StoreInfoDtoVo storeInfoDtoVo = FeignResponseUtil.getFeignData(feignStoreClient.getDtoVoByPxtId(s));
-                if (storeInfoDtoVo != null) {
-                    IceBox iceBox = new IceBox();
-                    IceBoxExtend iceBoxExtend = new IceBoxExtend();
+                SimpleSupplierInfoVo supplierInfoVo = FeignResponseUtil.getFeignData(feignSupplierClient.findByPxtId(Integer.valueOf(s)));
+
+                String storeNumber = "";
+                Integer deptId = null;
+                if (null != storeInfoDtoVo && StringUtils.isNotBlank(storeInfoDtoVo.getStoreNumber()) && null != supplierInfoVo && StringUtils.isNotBlank(supplierInfoVo.getNumber())) {
+                    list.add(s);
+                    continue;
+                } else {
+                    if (null != storeInfoDtoVo && StringUtils.isNotBlank(storeInfoDtoVo.getStoreNumber())) {
+                        storeNumber = storeInfoDtoVo.getStoreNumber();
+                        deptId = storeInfoDtoVo.getMarketArea();
+
+                    } else if (null != supplierInfoVo && StringUtils.isNotBlank(supplierInfoVo.getNumber())) {
+                        storeNumber = supplierInfoVo.getNumber();
+                        deptId = supplierInfoVo.getMarketAreaId();
+                    }
+                }
+                if (StringUtils.isNotBlank(storeNumber) && null != deptId) {
+
                     // 资产编号
                     String assetId = x.get(0).toString();
-                    // 冰柜名称
-                    String chestName = x.get(1).toString();
-                    // 品牌
-                    String brandName = x.get(2).toString();
-                    // 冰柜型号
-                    String modelName = x.get(3).toString();
-                    // 冰柜规格
-                    String chestNorm = x.get(4).toString();
 
-                    String storeNumber = storeInfoDtoVo.getStoreNumber();
-                    Integer deptId = storeInfoDtoVo.getMarketArea();
+                    Integer integer = iceBoxExtendDao.selectCount(Wrappers.<IceBoxExtend>lambdaQuery().eq(IceBoxExtend::getAssetId, assetId));
 
-                    // 投放的门店
-                    iceBox.setPutStoreNumber(storeNumber);
-                    // 冰柜所属部门id， 旧冰柜采用的是
-                    iceBox.setDeptId(deptId);
+                    if (integer == 0) {
+                        IceBox iceBox = new IceBox();
+                        IceBoxExtend iceBoxExtend = new IceBoxExtend();
 
-                    // 备注
-                    Object remarkObject = x.get(8);
+                        // 冰柜名称
+                        String chestName = x.get(1).toString();
+                        // 品牌
+                        String brandName = x.get(2).toString();
+                        // 冰柜型号
+                        String modelName = x.get(3).toString();
+                        // 冰柜规格
+                        String chestNorm = x.get(4).toString();
+                        // 投放的门店
+                        iceBox.setPutStoreNumber(storeNumber);
+                        // 冰柜所属部门id， 旧冰柜采用的是
+                        iceBox.setDeptId(deptId);
 
-                    if (remarkObject != null) {
-                        String remark = remarkObject.toString();
-                        if (StringUtils.isNotBlank(remark)) {
-                            iceBox.setRemark(remark);
+                        // 备注
+                        Object remarkObject = x.get(8);
+
+                        if (remarkObject != null) {
+                            String remark = remarkObject.toString();
+                            if (StringUtils.isNotBlank(remark)) {
+                                iceBox.setRemark(remark);
+                            }
                         }
-                    }
 
-                    // 押金
-                    Object depositMoneyObject = x.get(5);
-                    if (null != depositMoneyObject) {
-                        String depositMoney = depositMoneyObject.toString();
-                        if (StringUtils.isNotBlank(depositMoney)) {
-                            iceBox.setDepositMoney(new BigDecimal(depositMoney));
+                        // 押金
+                        Object depositMoneyObject = x.get(5);
+                        if (null != depositMoneyObject) {
+                            String depositMoney = depositMoneyObject.toString();
+
+                            if (StringUtils.isNotBlank(depositMoney)) {
+
+                                iceBox.setDepositMoney(new BigDecimal(depositMoney));
+                            }
                         }
-                    }
-                    // 投放日期
-                    String lastPutTime = x.get(9).toString();
-                    iceBoxExtend.setAssetId(assetId);
-                    iceBox.setChestName(chestName);
-                    iceBox.setBrandName(brandName);
-                    iceBox.setChestNorm(chestNorm);
-                    IceModel iceModel = iceModelDao.selectOne(Wrappers.<IceModel>lambdaQuery().eq(IceModel::getChestModel, modelName));
-                    if (null != iceModel) {
-                        iceBox.setModelId(iceModel.getId());
-                    }
-                    if (StringUtils.isNotBlank(lastPutTime)) {
-                        DateTime parse = DateUtil.parse(lastPutTime, "dd/MM/yyyy HH:mm:ss");
-                        iceBoxExtend.setLastPutTime(parse);
-                    }
+                        // 投放日期
+                        String lastPutTime = x.get(9).toString();
+                        iceBoxExtend.setAssetId(assetId);
+                        iceBox.setChestName(chestName);
+                        iceBox.setBrandName(brandName);
+                        iceBox.setChestNorm(chestNorm);
+                        IceModel iceModel = iceModelDao.selectOne(Wrappers.<IceModel>lambdaQuery().eq(IceModel::getChestModel, modelName));
+                        if (null != iceModel) {
+                            iceBox.setModelId(iceModel.getId());
+                        }
+                        if (StringUtils.isNotBlank(lastPutTime)) {
+                            DateTime parse = DateUtil.parse(lastPutTime, "yyyy-MM-dd HH:mm:ss");
+                            iceBoxExtend.setLastPutTime(parse);
+                        }
 
-                    iceBoxDao.insert(iceBox);
-                    iceBoxExtend.setId(iceBox.getId());
-                    iceBoxExtendDao.insert(iceBoxExtend);
+                        iceBoxDao.insert(iceBox);
+                        iceBoxExtend.setId(iceBox.getId());
+                        iceBoxExtendDao.insert(iceBoxExtend);
+                    }
                 }
             }
         }
         log.info("处理数据结束");
+        log.info("同时存在的数据-->[{}]", JSON.toJSONString(list, true));
         return new CommonResponse<>(Constants.API_CODE_SUCCESS, null);
     }
 

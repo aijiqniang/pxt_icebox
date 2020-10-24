16c9a38963238d4d527ea7d00d8244346a9fc2e8
cuimengyang <cuimengyang@szeastroc.com>
2020-10-24 17:22:17 +0800

    feat:增加逻辑

--- server/src/main/java/com/szeastroc/icebox/newprocess/enums/ResultEnum.java
+++ server/src/main/java/com/szeastroc/icebox/newprocess/enums/ResultEnum.java
@@ -10,7 +10,8 @@ public enum ResultEnum {
 
     DUPLICATE_ASSET_NUMBER(4101, "资产编号重复"),
     EXCEPTION_REPORTING(4102, "异常报备中,不能变更冰柜信息"),
-    CANNOT_CHANGE_CUSTOMER(4103,"正常的冰柜改为异常的冰柜时,不能变更使用客户")
+    CANNOT_CHANGE_CUSTOMER(4103,"正常的冰柜改为异常的冰柜时,不能变更使用客户"),
+    CANNOT_CHANGE_ICEBOX(4104,"不能变更冰柜信息")
     ;
 
     private final Integer code;

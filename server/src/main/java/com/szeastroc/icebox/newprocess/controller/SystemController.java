53d93741ea81c5a7bd499b537245d2e17462f17e
cuimengyang <cuimengyang@szeastroc.com>
2021-02-04 17:39:24 +0800

    feat：新增一个接口

---
+++ server/src/main/java/com/szeastroc/icebox/newprocess/controller/SystemController.java
@@ -0,0 +1,32 @@
+package com.szeastroc.icebox.newprocess.controller;
+
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.szeastroc.common.constant.Constants;
+import com.szeastroc.common.vo.CommonResponse;
+import com.szeastroc.icebox.newprocess.entity.IceBox;
+import com.szeastroc.icebox.newprocess.service.IceBoxService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+
+@Slf4j
+@RestController
+@RequestMapping("/system")
+public class SystemController {
+
+    @Resource
+    private IceBoxService iceBoxService;
+
+
+    @GetMapping("/heart")
+    public CommonResponse<IceBox> getSystemStatus() {
+
+        IceBox iceBox = iceBoxService.getOne(Wrappers.<IceBox>lambdaQuery().last("limit 1"));
+
+        return new CommonResponse<>(Constants.API_CODE_SUCCESS, null, iceBox);
+    }
+}

7097838f836e50058178e53ffab41f7e8019f2c7
chenchao <350655252@qq.com>
2021-03-05 10:57:10 +0800

    fix:冰柜巡检报表修改

--- server/src/main/java/com/szeastroc/icebox/newprocess/service/impl/IceInspectionReportServiceImpl.java
+++ server/src/main/java/com/szeastroc/icebox/newprocess/service/impl/IceInspectionReportServiceImpl.java
@@ -3,14 +3,31 @@ package com.szeastroc.icebox.newprocess.service.impl;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.szeastroc.common.entity.icebox.vo.IceInspectionReportMsg;
+import com.szeastroc.common.entity.user.vo.SessionDeptInfoVo;
+import com.szeastroc.common.entity.user.vo.SimpleUserInfoVo;
+import com.szeastroc.common.feign.customer.FeignStoreClient;
+import com.szeastroc.common.feign.customer.FeignSupplierClient;
+import com.szeastroc.common.feign.user.FeignCacheClient;
+import com.szeastroc.common.feign.user.FeignDeptClient;
+import com.szeastroc.common.feign.user.FeignUserClient;
+import com.szeastroc.common.utils.FeignResponseUtil;
 import com.szeastroc.icebox.newprocess.dao.IceInspectionReportDao;
+import com.szeastroc.icebox.newprocess.entity.IceBox;
 import com.szeastroc.icebox.newprocess.entity.IceInspectionReport;
+import com.szeastroc.icebox.newprocess.enums.DeptTypeEnum;
+import com.szeastroc.icebox.newprocess.service.IceBoxService;
+import com.szeastroc.icebox.newprocess.service.IceExamineService;
 import com.szeastroc.icebox.newprocess.service.IceInspectionReportService;
 import com.szeastroc.icebox.newprocess.vo.InspectionReportVO;
 import org.joda.time.DateTime;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 /**
  * 冰柜巡检报表 (TIceInspectionReport)表服务实现类
@@ -20,6 +37,21 @@ import java.util.List;
  */
 @Service
 public class IceInspectionReportServiceImpl extends ServiceImpl<IceInspectionReportDao, IceInspectionReport> implements IceInspectionReportService {
+
+    @Autowired
+    private IceBoxService iceBoxService;
+    @Autowired
+    private FeignStoreClient feignStoreClient;
+    @Autowired
+    private FeignSupplierClient feignSupplierClient;
+    @Autowired
+    FeignCacheClient feignCacheClient;
+    @Autowired
+    FeignDeptClient feignDeptClient;
+    @Autowired
+    FeignUserClient feignUserClient;
+    @Autowired
+    private IceExamineService iceExamineService;
 
     /**
      * 获取业务员当月报表
@@ -72,5 +104,285 @@ public class IceInspectionReportServiceImpl extends ServiceImpl<IceInspectionRep
     @Override
     public void truncate() {
         this.baseMapper.truncate();
+    }
+
+    @Transactional(rollbackFor = Exception.class, transactionManager = "transactionManager")
+    @Override
+    public void task(IceInspectionReportMsg reportMsg) {
+        switch (reportMsg.getOperateType()) {
+            case 1:
+                increasePutCount(reportMsg);
+                break;
+            case 2:
+                increaseInspectionCount(reportMsg);
+                break;
+            case 3:
+                buildReport(reportMsg.getUserId());
+                break;
+            case 4:
+                deleteReport(reportMsg);
+                break;
+            case 5:
+                recalculateLostScrapCount(reportMsg);
+                break;
+            case 6:
+                decreasePutCount(reportMsg);
+                break;
+            case 7:
+                updateDept(reportMsg);
+                break;
+            default:
+                break;
+        }
+    }
+
+
+    private void buildReport(Integer userId) {
+        if (Objects.isNull(userId)) {
+            return;
+        }
+        Integer deptId = FeignResponseUtil.getFeignData(feignDeptClient.getMainDeptByUserId(userId));
+        if (Objects.isNull(deptId)) {
+            return;
+        }
+        IceInspectionReport report = this.getCurrentMonthReport(userId);
+        List<Integer> putBoxIds = iceBoxService.getPutBoxIds(userId);
+        Integer inspectionCount = iceExamineService.getInspectionBoxes(putBoxIds).size();
+        int lostCount = iceBoxService.getLostScrapCount(putBoxIds);
+        if (Objects.isNull(report)) {
+            SimpleUserInfoVo userInfoVo = FeignResponseUtil.getFeignData(feignUserClient.findUserById(userId));
+            report = new IceInspectionReport();
+            report.setInspectionDate(new DateTime().toString("yyyy-MM"))
+                    .setUserId(userId)
+                    .setUserName(userInfoVo.getRealname());
+            Map<Integer, SessionDeptInfoVo> deptMap = FeignResponseUtil.getFeignData(feignCacheClient.getFiveLevelDept(deptId));
+            SessionDeptInfoVo headquarter = deptMap.get(5);
+            SessionDeptInfoVo business = deptMap.get(4);
+            if (!DeptTypeEnum.BUSINESS_UNIT.getType().equals(business.getDeptType())) {
+                business = null;
+                headquarter = deptMap.get(4);
+            }
+            SessionDeptInfoVo region = deptMap.get(3);
+            SessionDeptInfoVo service = deptMap.get(2);
+            SessionDeptInfoVo group = deptMap.get(1);
+            if (Objects.nonNull(headquarter)) {
+                report.setHeadquartersDeptId(headquarter.getId()).setHeadquartersDeptName(headquarter.getName());
+            }
+            if (Objects.nonNull(business)) {
+                report.setBusinessDeptId(business.getId()).setBusinessDeptName(business.getName());
+            }
+            if (Objects.nonNull(region)) {
+                report.setRegionDeptId(region.getId()).setRegionDeptName(region.getName());
+            }
+            if (Objects.nonNull(service)) {
+                report.setServiceDeptId(service.getId()).setServiceDeptName(service.getName());
+            }
+            if (Objects.nonNull(group)) {
+                report.setGroupDeptId(group.getId()).setGroupDeptName(group.getName());
+            }
+            report.setInspectionCount(inspectionCount).setLostScrapCount(lostCount).setPutCount(putBoxIds.size());
+            this.save(report);
+        } else {
+            report.setInspectionCount(inspectionCount).setLostScrapCount(lostCount).setPutCount(putBoxIds.size());
+            this.updateById(report);
+        }
+    }
+
+
+    /**
+     * 减少投放数
+     *
+     * @param reportMsg
+     */
+    private void decreasePutCount(IceInspectionReportMsg reportMsg) {
+        if (Objects.isNull(reportMsg.getUserId())) {
+            return;
+        }
+        IceInspectionReport currentMonthReport = this.getCurrentMonthReport(reportMsg.getUserId());
+        if (Objects.nonNull(currentMonthReport)) {
+            List<Integer> putBoxIds = iceBoxService.getPutBoxIds(reportMsg.getUserId());
+            currentMonthReport.setPutCount(putBoxIds.size());
+            int lostScrapCount = iceBoxService.getLostScrapCount(putBoxIds);
+            currentMonthReport.setLostScrapCount(lostScrapCount);
+            this.updateById(currentMonthReport);
+        }
+    }
+
+
+    /**
+     * 增加投放数量
+     *
+     * @param reportMsg
+     */
+    private void increasePutCount(IceInspectionReportMsg reportMsg) {
+        if (Objects.isNull(reportMsg.getBoxId())) {
+            return;
+        }
+        IceBox iceBox = iceBoxService.getById(reportMsg.getBoxId());
+        String storeNumber = iceBox.getPutStoreNumber();
+        Integer userId = FeignResponseUtil.getFeignData(feignStoreClient.getMainSaleManId(storeNumber));
+        if (Objects.isNull(userId)) {
+            userId = FeignResponseUtil.getFeignData(feignSupplierClient.getMainSaleManId(storeNumber));
+        }
+        if (Objects.nonNull(userId)) {
+            IceInspectionReport currentMonthReport = this.getCurrentMonthReport(userId);
+            if (Objects.nonNull(currentMonthReport)) {
+                List<Integer> putBoxIds = iceBoxService.getPutBoxIds(userId);
+                currentMonthReport.setPutCount(putBoxIds.size());
+                this.updateById(currentMonthReport);
+            } else {
+                currentMonthReport = new IceInspectionReport();
+                Integer deptId = FeignResponseUtil.getFeignData(feignDeptClient.getMainDeptByUserId(userId));
+                if (Objects.isNull(deptId)) {
+                    return;
+                }
+                Map<Integer, SessionDeptInfoVo> deptMap = FeignResponseUtil.getFeignData(feignCacheClient.getFiveLevelDept(deptId));
+                SessionDeptInfoVo headquarter = deptMap.get(5);
+                SessionDeptInfoVo business = deptMap.get(4);
+                if (!DeptTypeEnum.BUSINESS_UNIT.getType().equals(business.getDeptType())) {
+                    business = null;
+                    headquarter = deptMap.get(4);
+                }
+                SessionDeptInfoVo region = deptMap.get(3);
+                SessionDeptInfoVo service = deptMap.get(2);
+                SessionDeptInfoVo group = deptMap.get(1);
+                if (Objects.nonNull(headquarter)) {
+                    currentMonthReport.setHeadquartersDeptId(headquarter.getId()).setHeadquartersDeptName(headquarter.getName());
+                }
+                if (Objects.nonNull(business)) {
+                    currentMonthReport.setBusinessDeptId(business.getId()).setBusinessDeptName(business.getName());
+                }
+                if (Objects.nonNull(region)) {
+                    currentMonthReport.setRegionDeptId(region.getId()).setRegionDeptName(region.getName());
+                }
+                if (Objects.nonNull(service)) {
+                    currentMonthReport.setServiceDeptId(service.getId()).setServiceDeptName(service.getName());
+                }
+                if (Objects.nonNull(group)) {
+                    currentMonthReport.setGroupDeptId(group.getId()).setGroupDeptName(group.getName());
+                }
+                SimpleUserInfoVo userInfoVo = FeignResponseUtil.getFeignData(feignUserClient.findUserById(userId));
+                currentMonthReport.setInspectionDate(new DateTime().toString("yyyy-MM"))
+                        .setPutCount(1)
+                        .setUserId(userId)
+                        .setUserName(userInfoVo.getRealname());
+                this.save(currentMonthReport);
+            }
+        }
+    }
+
+    /**
+     * 增加巡检数量
+     *
+     * @param reportMsg
+     */
+    private void increaseInspectionCount(IceInspectionReportMsg reportMsg) {
+        if (Objects.isNull(reportMsg.getBoxId())) {
+            return;
+        }
+        int examineCount = iceExamineService.getExamineCount(reportMsg.getBoxId());
+        if (examineCount <= 1) {
+            IceBox iceBox = iceBoxService.getById(reportMsg.getBoxId());
+            String storeNumber = iceBox.getPutStoreNumber();
+            Integer userId = FeignResponseUtil.getFeignData(feignStoreClient.getMainSaleManId(storeNumber));
+            if (Objects.isNull(userId)) {
+                userId = FeignResponseUtil.getFeignData(feignSupplierClient.getMainSaleManId(storeNumber));
+            }
+            if (Objects.nonNull(userId)) {
+                IceInspectionReport currentMonthReport = this.getCurrentMonthReport(userId);
+                if (Objects.nonNull(currentMonthReport)) {
+                    currentMonthReport.setInspectionCount(currentMonthReport.getInspectionCount() + 1);
+                    this.updateById(currentMonthReport);
+                }
+            }
+        }
+    }
+
+
+    /**
+     * 重新计算遗失报废数量
+     *
+     * @param reportMsg
+     */
+    private void recalculateLostScrapCount(IceInspectionReportMsg reportMsg) {
+        if (Objects.isNull(reportMsg.getBoxId())) {
+            return;
+        }
+        IceBox iceBox = iceBoxService.getById(reportMsg.getBoxId());
+        String storeNumber = iceBox.getPutStoreNumber();
+        Integer userId = FeignResponseUtil.getFeignData(feignStoreClient.getMainSaleManId(storeNumber));
+        if (Objects.isNull(userId)) {
+            userId = FeignResponseUtil.getFeignData(feignSupplierClient.getMainSaleManId(storeNumber));
+        }
+        if (Objects.nonNull(userId)) {
+            IceInspectionReport currentMonthReport = this.getCurrentMonthReport(userId);
+            if (Objects.nonNull(currentMonthReport)) {
+                int lostScrapCount = iceBoxService.getLostScrapCount(userId);
+                currentMonthReport.setLostScrapCount(lostScrapCount);
+                this.updateById(currentMonthReport);
+            }
+        }
+
+    }
+
+    /**
+     * 删除报表数据（人员离职）
+     *
+     * @param reportMsg
+     */
+    private void deleteReport(IceInspectionReportMsg reportMsg) {
+        if (Objects.isNull(reportMsg.getUserId())) {
+            return;
+        }
+        LambdaQueryWrapper<IceInspectionReport> wrapper = new LambdaQueryWrapper<IceInspectionReport>()
+                .eq(IceInspectionReport::getUserId, reportMsg.getUserId()).eq(IceInspectionReport::getInspectionDate, new DateTime().toString("yyyy-MM"));
+        this.remove(wrapper);
+    }
+
+    /**
+     * 更新人员部门
+     *
+     * @param reportMsg
+     */
+    private void updateDept(IceInspectionReportMsg reportMsg) {
+        if (Objects.isNull(reportMsg.getUserId())) {
+            return;
+        }
+        LambdaQueryWrapper<IceInspectionReport> wrapper = Wrappers.<IceInspectionReport>lambdaQuery();
+        wrapper.eq(IceInspectionReport::getUserId, reportMsg.getUserId()).eq(IceInspectionReport::getInspectionDate, new DateTime().toString("yyyy-MM"));
+        IceInspectionReport one = this.getOne(wrapper);
+        if (Objects.isNull(one)) {
+            return;
+        }
+        Integer deptId = FeignResponseUtil.getFeignData(feignDeptClient.getMainDeptByUserId(reportMsg.getUserId()));
+        if (Objects.isNull(deptId)) {
+            return;
+        }
+        Map<Integer, SessionDeptInfoVo> deptMap = FeignResponseUtil.getFeignData(feignCacheClient.getFiveLevelDept(deptId));
+        SessionDeptInfoVo headquarter = deptMap.get(5);
+        SessionDeptInfoVo business = deptMap.get(4);
+        if (!DeptTypeEnum.BUSINESS_UNIT.getType().equals(business.getDeptType())) {
+            business = null;
+            headquarter = deptMap.get(4);
+        }
+        SessionDeptInfoVo region = deptMap.get(3);
+        SessionDeptInfoVo service = deptMap.get(2);
+        SessionDeptInfoVo group = deptMap.get(1);
+        if (Objects.nonNull(headquarter)) {
+            one.setHeadquartersDeptId(headquarter.getId()).setHeadquartersDeptName(headquarter.getName());
+        }
+        if (Objects.nonNull(business)) {
+            one.setBusinessDeptId(business.getId()).setBusinessDeptName(business.getName());
+        }
+        if (Objects.nonNull(region)) {
+            one.setRegionDeptId(region.getId()).setRegionDeptName(region.getName());
+        }
+        if (Objects.nonNull(service)) {
+            one.setServiceDeptId(service.getId()).setServiceDeptName(service.getName());
+        }
+        if (Objects.nonNull(group)) {
+            one.setGroupDeptId(group.getId()).setGroupDeptName(group.getName());
+        }
+        this.updateById(one);
     }
 }

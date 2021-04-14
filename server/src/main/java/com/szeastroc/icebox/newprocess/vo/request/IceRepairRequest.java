817c675a5b19499e8e5b870528cca324f7f32f2c
chenchao <350655252@qq.com>
2021-04-14 19:11:51 +0800

    fix:冰柜包修指定城市的行政区域编码

--- server/src/main/java/com/szeastroc/icebox/newprocess/vo/request/IceRepairRequest.java
+++ server/src/main/java/com/szeastroc/icebox/newprocess/vo/request/IceRepairRequest.java
@@ -20,51 +20,50 @@ public class IceRepairRequest {
     private String psnAccount;
     private String psnPwd;
     private String originFlag;
-    @ApiModelProperty(value = "冰柜型号",required = true)
+    @ApiModelProperty(value = "冰柜型号", required = true)
     private String modelName;
     private String saleOrderId;
-    @ApiModelProperty(value = "预约日期",required = true)
+    @ApiModelProperty(value = "预约日期", required = true)
     private String requireServiceDate;
-    @ApiModelProperty(value = "姓名",required = true)
+    @ApiModelProperty(value = "姓名", required = true)
     private String linkMan;
-    @ApiModelProperty(value = "手机号",required = true)
+    @ApiModelProperty(value = "手机号", required = true)
     private String linkMobile;
-    @ApiModelProperty(value = "时间范围",required = true)
+    @ApiModelProperty(value = "时间范围", required = true)
     private String bookingRange;
     private String serviceTypeId;
-    @ApiModelProperty(value = "客户（门店/配送商）编号",required = true,example = "C0000001")
+    @ApiModelProperty(value = "客户（门店/配送商）编号", required = true, example = "C0000001")
     private String customerNumber;
-    @ApiModelProperty(value = "客户名称",required = true)
+    @ApiModelProperty(value = "客户名称", required = true)
     private String customerName;
-    @ApiModelProperty(value = "客户地址",required = true)
+    @ApiModelProperty(value = "客户地址", required = true)
     private String customerAddress;
-    @ApiModelProperty(value = "冰箱id",required = true)
+    @ApiModelProperty(value = "冰箱id", required = true)
     private Integer boxId;
-    @ApiModelProperty(value = "资产编号",required = true)
+    @ApiModelProperty(value = "资产编号", required = true)
     private String assetId;
-    @ApiModelProperty(value = "客户类型",required = true,notes = "1经销商 2分销商 3邮差 4批发 5门店",example = "5")
+    @ApiModelProperty(value = "客户类型", required = true, notes = "1经销商 2分销商 3邮差 4批发 5门店", example = "5")
     private Integer customerType;
-    @ApiModelProperty(value = "维修备注",required = true)
+    @ApiModelProperty(value = "维修备注", required = true)
     private String remark;
-    @ApiModelProperty(value = "问题类型",required = true)
+    @ApiModelProperty(value = "问题类型", required = true)
     private String description;
-    @ApiModelProperty(value = "冰箱类型id",required = true)
+    @ApiModelProperty(value = "冰箱类型id", required = true)
     private Integer modelId;
-    @ApiModelProperty(value = "省份",required = true)
+    @ApiModelProperty(value = "省份", required = true)
     private String province;
-    @ApiModelProperty(value = "省份编码",required = true)
+    @ApiModelProperty(value = "省份编码", required = true)
     private String provinceCode;
-    @ApiModelProperty(value = "城市",required = true)
+    @ApiModelProperty(value = "城市", required = true)
     private String city;
-    @ApiModelProperty(value = "城市编码",required = true)
+    @ApiModelProperty(value = "城市编码", required = true)
     private String cityCode;
-    @ApiModelProperty(value = "区县",required = true)
+    @ApiModelProperty(value = "区县", required = true)
     private String area;
-    @ApiModelProperty(value = "区县编码",required = true)
+    @ApiModelProperty(value = "区县编码", required = true)
     private String areaCode;
 
     private String phoneAreaCode;
-
 
 
     public WbSiteRequestVO convertToWbSite() {
@@ -73,19 +72,28 @@ public class IceRepairRequest {
         requestVO.setPsnAccount(objectFactory.createWbSiteRequestVOPsnAccount(this.psnAccount));
         requestVO.setPsnPwd(objectFactory.createWbSiteRequestVOPsnPwd(this.psnPwd));
         requestVO.setOriginFlag(objectFactory.createWbSiteRequestVOOriginFlag(this.originFlag));
-        if("SC-200-1".equals(this.modelName)){
+        if ("SC-200-1".equals(this.modelName)) {
             requestVO.setModelName(objectFactory.createWbSiteRequestVOModelName("SC-200"));
-        }else{
+        } else {
             requestVO.setModelName(objectFactory.createWbSiteRequestVOModelName(this.modelName));
         }
         requestVO.setSaleOrderId(objectFactory.createWbSiteRequestVOSaleOrderId(this.saleOrderId));
         requestVO.setTelephone2(objectFactory.createWbSiteRequestVOTelephone2(this.linkMobile));
         requestVO.setAddress(objectFactory.createWbSiteRequestVOAddress(this.customerAddress));
-        if(this.areaCode.length()>6){
-            requestVO.setRegoinId(objectFactory.createWbSiteRequestVORegoinId(this.areaCode.substring(0,6)+"000"));
-        }else{
-            requestVO.setRegoinId(objectFactory.createWbSiteRequestVORegoinId(this.areaCode+"000"));
+        if (this.city.contains("儋州")) {
+            requestVO.setRegoinId(objectFactory.createWbSiteRequestVORegoinId("469003000"));
+        } else if (this.city.contains("中山")) {
+            requestVO.setRegoinId(objectFactory.createWbSiteRequestVORegoinId("442000000"));
+        } else if (this.city.contains("东莞")) {
+            requestVO.setRegoinId(objectFactory.createWbSiteRequestVORegoinId("441904100"));
+        } else {
+            if (this.areaCode.length() > 6) {
+                requestVO.setRegoinId(objectFactory.createWbSiteRequestVORegoinId(this.areaCode.substring(0, 6) + "000"));
+            } else {
+                requestVO.setRegoinId(objectFactory.createWbSiteRequestVORegoinId(this.areaCode + "000"));
+            }
         }
+
         requestVO.setRequireServiceDate(objectFactory.createWbSiteRequestVORequireServiceDate(this.requireServiceDate));
         requestVO.setFaultDesc(objectFactory.createWbSiteRequestVOFaultDesc(this.description));
         requestVO.setCustomerName(objectFactory.createWbSiteRequestVOCustomerName(this.linkMan));

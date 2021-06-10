f6c368bae5aadce069db2cd401b3ee48674e5318
aijinqiang <aijinqiang@szeastroc.com>
2021-06-10 14:08:23 +0800

    冰柜交接

---
+++ server/src/main/java/com/szeastroc/icebox/newprocess/entity/IceBoxHandover.java
@@ -0,0 +1,11 @@
+package com.szeastroc.icebox.newprocess.entity;
+
+/**
+ * TODO
+ *
+ * @author Aijinqiang
+ * @version 1.0
+ * @date 2021/6/10 11:46
+ */
+public class IceBoxHandover {
+}
---
+++ server/src/main/java/com/szeastroc/icebox/newprocess/vo/IceBoxHandoverPage.java
@@ -0,0 +1,11 @@
+package com.szeastroc.icebox.newprocess.vo;
+
+/**
+ * TODO
+ *
+ * @author Aijinqiang
+ * @version 1.0
+ * @date 2021/6/10 11:47
+ */
+public class IceBoxHandoverPage {
+}

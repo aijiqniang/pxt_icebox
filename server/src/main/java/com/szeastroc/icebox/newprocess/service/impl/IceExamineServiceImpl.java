f0b4729f632d126e820b34523cc165e51b39a081
aijinqiang <aijinqiang@szeastroc.com>
2021-08-06 11:02:12 +0800

    fix：退冰柜有部门的服务处为空导致空指针异常

--- server/src/main/java/com/szeastroc/icebox/newprocess/service/impl/IceExamineServiceImpl.java
+++ server/src/main/java/com/szeastroc/icebox/newprocess/service/impl/IceExamineServiceImpl.java
@@ -2,6 +2,7 @@ package com.szeastroc.icebox.newprocess.service.impl;
 
 import cn.hutool.core.collection.CollectionUtil;
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.core.toolkit.CollectionUtils;

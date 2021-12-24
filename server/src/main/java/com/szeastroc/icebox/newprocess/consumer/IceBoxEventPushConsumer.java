1c8988adecff8a250f5d7a41ddc4d02f5c5ea43c
aijinqiang <aijinqiang@szeastroc.com>
2021-12-24 10:50:01 +0800

    fix: 异步消费

--- server/src/main/java/com/szeastroc/icebox/newprocess/consumer/IceBoxEventPushConsumer.java
+++ server/src/main/java/com/szeastroc/icebox/newprocess/consumer/IceBoxEventPushConsumer.java
@@ -1,6 +1,7 @@
 package com.szeastroc.icebox.newprocess.consumer;
 
 import com.alibaba.fastjson.JSONObject;
+import com.szeastroc.common.utils.ExecutorServiceFactory;
 import com.szeastroc.icebox.config.MqConstant;
 import com.szeastroc.icebox.oldprocess.service.IceEventRecordService;
 import com.szeastroc.icebox.oldprocess.vo.HisenseDTO;
@@ -8,6 +9,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.concurrent.CompletableFuture;
 
 
 @Slf4j
@@ -20,7 +23,11 @@ public class IceBoxEventPushConsumer {
     public void task(HisenseDTO hisenseDTO) {
 
         log.info("冰柜推送事件触发，消息{}", JSONObject.toJSONString(hisenseDTO));
-        iceEventRecordService.eventPushConsumer(hisenseDTO);
+        try{
+            CompletableFuture.runAsync(()->iceEventRecordService.eventPushConsumer(hisenseDTO), ExecutorServiceFactory.getInstance());
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
 }

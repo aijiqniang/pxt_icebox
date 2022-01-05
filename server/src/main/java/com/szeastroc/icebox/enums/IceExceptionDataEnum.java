bd3e6a914b19239c8e2e6ba5eb0cd00257641950
liangdx <liangdingxiang@szeastroc.com>
2022-01-05 14:55:31 +0800

    update:冰柜异常数据修改 系统新增 and 系统新增&报废 and 系统新增&遗失

--- server/src/main/java/com/szeastroc/icebox/enums/IceExceptionDataEnum.java
+++ server/src/main/java/com/szeastroc/icebox/enums/IceExceptionDataEnum.java
@@ -10,8 +10,22 @@ public enum IceExceptionDataEnum {
     ICE_SCRAP(2, "报废"),
     ICE_LOSE(3, "遗失"),
     ICE_ADD(4, "新增"),
-    ICE_RETURN_SCRAP(5, "退仓&报废");
+    ICE_RETURN_SCRAP(5, "退仓&报废"),
+    ICE_SYSTEM_ADD(6, "系统新增"),
+    ICE_ADD_SCRAP(7, "系统新增&报废"),
+    ICE_ADD_LOSE(8, "系统新增&遗失");
 
     private Integer type;
     private String desc;
+
+
+
+    public static Integer getEnumType(String desc) {
+        for (IceExceptionDataEnum enu : IceExceptionDataEnum.values()) {
+            if (enu.getDesc().equals(desc)) {
+                return enu.getType();
+            }
+        }
+        return null;
+    }
 }
